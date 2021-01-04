3a8e7ff9da9d3f17b21b115f9eeb1a252ea003cb
Qi Zhao <qiblaqi@126.com>
2021-01-04 16:20:55 -0500

    Update HW10

--- Student_Repository_Qi_Zhao_Test.py
+++ Student_Repository_Qi_Zhao_Test.py
@@ -1,14 +1,17 @@
 """
-This is HW10 unittest scripts. Written By Qi Zhao. Testing the following methods and class: date_arithmetic()-> Tuple[datetime, datetime, int]
-file_reader(str, int, str, bool) -> Iterator[List[str]], class FileAnalyzer
+This is HW10 unittest scripts. Written By Qi Zhao. Testing the following methods and class: HW10
 """
 import unittest
 
 from Student_Repository_Qi_Zhao import *
+from typing import List, Tuple, Iterator, Dict
 
 class HW10Test(unittest.TestCase):
-    def test_students(self) -> None:
-        pass
+    test_path:str = os.path.dirname(__file__)
+    
+    def test_grades(self) -> None:
+        stevens_data: "HW10" = HW10(self.test_path, "Stevens")
+        stevens_data.pretty_print()
     
 if __name__ == '__main__':
     unittest.main()
