e7f7df099109fbd3a5b4600a4fd67bc0dd7fd8c6
Qi Zhao <qiblaqi@126.com>
2021-01-04 14:55:41 -0500

    Upload HW08

--- Student_Repository_Qi_Zhao.py
+++ Student_Repository_Qi_Zhao.py
@@ -2,8 +2,9 @@
 This is HW08 Written By Qi Zhao. This file contains following functions: date_arithmetic()-> Tuple[datetime, datetime, int]
 file_reader(str, int, str, bool) -> Iterator[List[str]], class FileAnalyzer
 """
-from typing import List, Tuple, Iterator, Dict
-from datetime import datetime, timedelta
+import os
+from typing import List, Tuple, Iterator, Dict, IO
+from datetime import datetime, time, timedelta
 from prettytable import PrettyTable
 
 def date_arithmetic() -> Tuple[datetime, datetime, int]:
@@ -12,9 +13,9 @@ def date_arithmetic() -> Tuple[datetime, datetime, int]:
         What is the date three days after Feb 27, 2019?
         How many days passed between Feb 1, 2019 and Sept 30, 2019?
     """
-    three_days_after_02272020: datetime = # your code goes here for calculation
-    three_days_after_02272019: datetime = # your code goes here for calculation
-    days_passed_02012019_09302019: int = # your code goes here for calculation
+    three_days_after_02272020: datetime = datetime(2020,2,27) + timedelta(days=3)
+    three_days_after_02272019: datetime = datetime(2019,2,27) + timedelta(days=3)
+    days_passed_02012019_09302019: int = (datetime(2019,9,30) - datetime(2019,2,1)).days
 
     return three_days_after_02272020, three_days_after_02272019, days_passed_02012019_09302019
  
@@ -22,7 +23,20 @@ def file_reader(path: str, fields:int, sep=',', header=False) -> Iterator[List[s
     """ file_reader() to read field-separated text files and yield a tuple with all of the values 
         from a single line in the file on each call to next()
     """
-    pass
+    try:
+        fp = open(path,'r')
+    except FileNotFoundError:
+        raise
+    with fp:
+        line_count: int = 0
+        for my_line in fp:
+            line_count += 1
+            my_line = my_line.strip('\n')
+            my_list:list = [ item for item in my_line.split(',') ]
+            if len(my_list) != fields:
+                raise ValueError(f"{path} has {len(my_list)} fields on line {line_count} but expected {fields}")
+            else:
+                yield [ item for item in my_line.split(',') ]
 
 class FileAnalyzer:
     """ FileAnalyzer that given a directory name, searches that directory for Python files """
@@ -34,9 +48,46 @@ class FileAnalyzer:
         self.analyze_files() # summerize the python files data
 
     def analyze_files(self) -> None:
-        """ a method that populate the summarized data into self.files_summary"""
-        pass # implement your code here
+        """ a method that populate the summarized data into Dictionary self.files_summary"""
+        for file in os.listdir(self.directory):
+            if file.endswith(".py"):
+                
+                try:
+                    fp:IO = open(file, 'r')
+                except FileNotFoundError:
+                    raise
+                with fp:
+                    my_class:int = 0
+                    my_func:int = 0
+                    my_line:int = 0
+                    my_char:int = 0
+                    my_file_lines: List = fp.readlines()
+                    for line in my_file_lines:
+                        if line.startswith("class "):
+                            my_class += 1
+                        elif line.startswith("def ") or line.startswith("    def "):
+                            my_func += 1
+                        else:
+                            continue
+                    my_line = len(my_file_lines)
+                    my_char = len("".join(my_file_lines))
+                    self.files_summary[file] = {
+                        "class": my_class,
+                        "function": my_func,
+                        "line": my_line,
+                        "char": my_char
+                    }
+            else:
+                continue
+        
 
     def pretty_print(self) -> None:
         """ a method that print out the pretty table from the data stored in the self.files_summary."""
-        pass # implement your code here
+        x = PrettyTable()
+        x.field_names = ["file name", "class", "function", "line", "char"]
+        for item1, items2 in self.files_summary.items():
+            my_row = []
+            my_row.append(item1)
+            my_row.extend(items2.values())
+            x.add_row(my_row)
+        print(x)
--- Student_Repository_Qi_Zhao_Test.py
+++ Student_Repository_Qi_Zhao_Test.py
@@ -3,12 +3,65 @@ This is HW08 unittest scripts. Written By Qi Zhao. Testing the following methods
 file_reader(str, int, str, bool) -> Iterator[List[str]], class FileAnalyzer
 """
 import unittest
+import os
 
-# from HW08_Qi_Zhao import *
+from typing import Dict
+from HW08_Qi_Zhao import *
 
 class HW08Test(unittest.TestCase):
     def test_date_arithmetic(self) -> None:
-        pass
+        self.assertTrue(date_arithmetic() == (datetime(2020,3,1), datetime(2019,3,2), 241))
+        
+    def test_file_reader(self) -> None:
+        my_path: str = os.path.dirname(__file__)
+        file_path: str = os.path.join(my_path, "hw08_test.txt")
+        foo_path: str = os.path.join(my_path, "hw08_foo.txt")
+        error_file_path: str = os.path.join(my_path, "hw08_test_error.txt")
+        self.assertEqual(list(file_reader(file_path, 3)), [["CWID","Name","Major"],["123","Jin He","Computer Science"],
+                                                        ["234","Nanda Koka","Software Engineering"],
+                                                        ["345","Benji Cai","Software Engineering"]])
+        with self.assertRaises(ValueError) as context:
+            list(file_reader(error_file_path, 3))
+        self.assertTrue("hw08_test_error.txt has 4 fields on line 1 but expected 3" in str(context.exception))
+        with self.assertRaises(FileNotFoundError):
+            list(file_reader(foo_path, 3))
+            
+class HW08FileAnalyzerTest(unittest.TestCase):
+    my_path:str = os.path.dirname(__file__)
+    foo_path:str = "/foo"
+    def test_analyze_files(self) -> None:
+        fa = FileAnalyzer(self.my_path)
+        result: Dict[str, Dict[str, int]] = {
+        '0_defs_in_this_file.py': {
+            'class': 0,
+            'function': 0,
+            'line': 3,
+            'char': 57
+            }, 
+        'file1.py': {
+            'class': 2,
+            'function': 4,
+            'line': 25,
+            'char': 270
+            },
+        'HW08_Qi_Zhao.py': {
+            'class': 1,
+            'function': 5,
+            'line': 93,
+            'char': 4026
+            },
+        'HW08_Test_Qi_Zhao.py': {
+            'class': 2,
+            'function': 4,
+            'line': 67,
+            'char': 2469
+            },
+        }
+        self.assertEqual(fa.files_summary, result)
+    
+    def test_pretty_print(self) -> None:
+        fa = FileAnalyzer(self.my_path)
+        fa.pretty_print()
     
 if __name__ == '__main__':
     unittest.main()
